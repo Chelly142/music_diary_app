c7b0d7851866f843f39139a97d10ca6ae702b2aa
Chelly142 <67005998+Chelly142@users.noreply.github.com>
2020-08-12 01:40:28 +0900

    second commit

--- app/src/main/java/com/zoe1/music_diary/Activity/MainActivity.java
+++ app/src/main/java/com/zoe1/music_diary/Activity/MainActivity.java
@@ -1,8 +1,13 @@
 package com.zoe1.music_diary.Activity;
 
+import androidx.annotation.DrawableRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +16,7 @@ import android.widget.TextView;
 
 import com.google.android.material.tabs.TabLayout;
 import com.zoe1.music_diary.R;
+import com.zoe1.music_diary.TabPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
     private Context mContext;
@@ -21,19 +27,51 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mContext = getApplicationContext();
         tabLayout = findViewById(R.id.tab_layout);
-        int music_count = 0;
-        int diary_count = 0;
-        int category_count = 0;
+        int music_count = 100;
+        int diary_count = 100;
+        int category_count = 100;
         tabLayout.addTab(tabLayout.newTab().setCustomView(createTabView("곡",music_count)));
         tabLayout.addTab(tabLayout.newTab().setCustomView(createTabView("일기",diary_count)));
         tabLayout.addTab(tabLayout.newTab().setCustomView(createTabView("장르",category_count)));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        final ViewPager viewPager = findViewById(R.id.view_pager);
+        TabPagerAdapter tabPagerAdapter = new TabPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(tabPagerAdapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                TextView menu_name_text = tab.getCustomView().findViewById(R.id.tab_name_text);
+                TextView count_text = tab.getCustomView().findViewById(R.id.count_text);
+                Drawable d = getResources().getDrawable(R.drawable.blue_textview_round_custom);
+                count_text.setBackground(d);
+                menu_name_text.setTextColor(Color.parseColor("#001BFF"));
+                count_text.setTextColor(Color.parseColor("#001BFF"));
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                TextView menu_name_text = tab.getCustomView().findViewById(R.id.tab_name_text);
+                TextView count_text = tab.getCustomView().findViewById(R.id.count_text);
+                Drawable d = getResources().getDrawable(R.drawable.textview_round_custom);
+                count_text.setBackground(d);
+                menu_name_text.setTextColor(Color.parseColor("#FFFFFF"));
+                count_text.setTextColor(Color.parseColor("#FFFFFF"));
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
     private View createTabView(String tabName,Integer count) {
         View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab, null);
-        TextView menu_name_text =  tabView.findViewById(R.id.menu_name_text);
+        TextView menu_name_text =  tabView.findViewById(R.id.tab_name_text);
         TextView count_text = tabView.findViewById(R.id.count_text);
         menu_name_text.setText(tabName);
-        count_text.setText("("+count+")");
+        count_text.setText(count+"");
         return tabView;
     }
 }
---
+++ app/src/main/java/com/zoe1/music_diary/TabPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.zoe1.music_diary;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.zoe1.music_diary.Fragment.Activitiy.categoryFragment;
+import com.zoe1.music_diary.Fragment.Activitiy.diaryFragment;
+import com.zoe1.music_diary.Fragment.Activitiy.musicFragment;
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+    private  int tabCount;
+    public TabPagerAdapter(@NonNull FragmentManager fm, int tabCount) {
+        super(fm, tabCount);
+        this.tabCount = tabCount;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                musicFragment musicFragment = new musicFragment();
+                return  musicFragment;
+            case 1:
+                diaryFragment diaryFragment = new diaryFragment();
+                return diaryFragment;
+            case 2:
+                categoryFragment categoryFragment = new categoryFragment();
+                return categoryFragment;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
